d595aee5f3f9419d7e77458c08c338c16315ffb0
leon_feng <b_feng@ucsb.edu>
2023-04-12 17:47:17 -0700

    add rapid city route

---
+++ frontend/src/main/pages/RapidCityPage.js
@@ -0,0 +1,22 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+
+const RapidCityPage = () => {
+    return (
+        <BasicLayout>
+            <div>
+                <h1>Rapid City, SD</h1>
+                <p>
+                Rapid City lies east of Black Hills National Forest in western South Dakota. 
+                It’s known as a gateway to Mt. Rushmore, the massive iconic sculpture of 4 U.S. presidents. 
+                “City of Presidents,” a series of life-size statues, spans several blocks downtown. 
+                North of Rapid Creek, which bisects town, the Journey Museum & Learning Center offers local history and geology exhibits. 
+                Snakes are on view at Reptile Gardens zoo.
+                </p>
+                <p>Added by Leon F.</p>
+            </div>
+        </BasicLayout>
+    )
+}
+
+export default RapidCityPage;
