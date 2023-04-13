86ff50ff24404a2cea1a66f72379702f213b1c22
Tim Qin <yqin@ucsb.edu>
2023-04-12 17:47:48 -0700

    tq - added page and menu item for San Francisco

---
+++ frontend/src/main/pages/SanFranciscoPage.js
@@ -0,0 +1,15 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function SanFranciscoPage() {
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>San Francisco, CA</h1>
+        <p>
+        San Francisco, officially the City and County of San Francisco, is a commercial, financial, and cultural center of Northern California.
+        </p>
+        <p>Added by Tim Q.</p>
+      </div>
+    </BasicLayout>
+  )
+}
