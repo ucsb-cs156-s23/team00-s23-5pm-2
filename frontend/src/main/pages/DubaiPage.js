07ffa6b42f47a0e0d9cb4426de9be2d3a63fddea
Cheryl Stanley <cherylstanley@ucsb.edu>
2023-04-12 17:26:51 -0700

    cs - add page for dubai

---
+++ frontend/src/main/pages/DubaiPage.js
@@ -0,0 +1,16 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function DubaiPage() {
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Dubai, United Arab Emirates</h1>
+        <p>
+         <b>Dubai, United Arab Emirates</b> is a city in United Arab Emirates,located on the West coast of the Arabian peninsula and is one of seven emirates located within the United Arab Emirates.
+         Dubai contains skyscrapers, ports, and beaches. It currently contains the tallest man-made skyscraper in the world, the Burj Khalifa, which stands at an impressive 163 floors and 2,722 feet.
+        </p>
+        <p>Added by Cheryl S.</p>
+      </div>
+    </BasicLayout>
+  )
+}
