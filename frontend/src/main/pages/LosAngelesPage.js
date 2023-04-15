c45677dddbb3ac2e42ea9fdff8f1262d5078be97
jtmnguyen <jessicanguyen@ucsb.edu>
2023-04-14 17:53:01 -0700

    xy - add page for Los Angeles

---
+++ frontend/src/main/pages/LosAngelesPage.js
@@ -0,0 +1,16 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function LosAngelesPage() {
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Los Angeles, CA</h1>
+        <p>
+         <b>Los Angeles, California</b> is the largest city in the state of California.
+         It is located in Southern California and is most famous for its TV, movie, and music industries.
+        </p>
+        <p>Added by Jessica N.</p>
+      </div>
+    </BasicLayout>
+  )
+}
