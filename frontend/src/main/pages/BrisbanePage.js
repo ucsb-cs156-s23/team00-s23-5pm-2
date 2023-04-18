b7fd2e8887bfe5973f0b9cef80cbb10a01a48599
Simon Lai <simonlai@ucsb.edu>
2023-04-17 21:38:23 -0700

    cg - added page for Brisbane

---
+++ frontend/src/main/pages/BrisbanePage.js
@@ -0,0 +1,16 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function BrisbanePage() {
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Brisbane, AU</h1>
+        <p>
+         <b>Brisbane, Australia</b> is the capital and most populous city of Queensland, and the third-most populous city in Australia and Oceania with a population of approximately 2.6 million.
+         Brisbane lies at the centre of South East Queensland, which includes several other regional centres and cities.
+        </p>
+        <p>Added by Simon L.</p>
+      </div>
+    </BasicLayout>
+  )
+}
